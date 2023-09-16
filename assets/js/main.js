aa9de19c2ad00ed2d557a134e9a00c78a5b564de
AlyciaSilva <alyciasilvaprogrammer@gmail.com>
2023-09-16 15:41:57 -0300

    Adicionando buttom de paginação

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,29 @@
-function convertPokemonToHtmlLi(pokemon) {
-    return `
-    <li class="pokemon ${pokemon.type}">
-                <span class="number">#${pokemon.number}</span>
-                <span class="name">${pokemon.name} </span>
-               <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}" >${type}</li>`).join('')}
-                </ol>
-                <img src="${pokemon.photo}" 
-                alt="${pokemon.name}">
-               </div>  
-            </li>  
-    `        
-}
-
 const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadmoreButtom')
+const limit = 5
+const offset = 0
+
 
 //pokemonList.innerHTML += '<li>AAAAAAAAAAAAAAAAAAAAAAA</li>' 
-pokeApi.getPokemons().then((pokemons = []) => {
-    const newHtml = pokemons.map(convertPokemonToHtmlLi).join('')  
-    pokemonList.innerHTML = newHtml 
+function loadPokemonItens(offset,limit){
+    function convertPokemonToHtmlLi(pokemon) {
+        return `
+        <li class="pokemon ${pokemon.type}">
+                    <span class="number">#${pokemon.number}</span>
+                    <span class="name">${pokemon.name} </span>
+                   <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}" >${type}</li>`).join('')}
+                    </ol>
+                    <img src="${pokemon.photo}" 
+                    alt="${pokemon.name}">
+                   </div>  
+                </li>  
+        `        
+    }
+    pokeApi.getPokemons(offset,limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonToHtmlLi).join('')  
+        pokemonList.innerHTML += newHtml 
         /*
         const newListItems = pokemon.map((value) =>{
             return convertPokemonToHtmlLi(pokemon)
@@ -35,4 +39,8 @@ pokeApi.getPokemons().then((pokemons = []) => {
         Sintaxe alternativa sem o uso do map() */
            
     })
+}
 
+loadMoreButton.addEventListener('click', () => {
+    loadPokemonItens()
+})
